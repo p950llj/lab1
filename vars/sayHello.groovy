dc2c51812a5f7227e92c63c15da476b3e2f448cb
p950llj <p950llj@swedbank.se>
2023-04-02 14:14:10 +0200

    stuff

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,3 +1,4 @@
+//https://phoenixnap.com/kb/jenkins-shared-library
 // vars/sayHello.groovy
 def call(String name = 'human') {
     // Any valid steps can be called from this code, just like in other
